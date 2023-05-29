41f86bd0ae8bcef626d748ead753fcf652de6fce
Russ <qatsby@yandex.com>
2023-05-29 00:22:42 -0400

    updated

---
+++ src/main/java/constants/EndPoints.java
@@ -0,0 +1,11 @@
+package constants;
+
+public class EndPoints {
+    public static final String URL = "https://stellarburgers.nomoreparties.site/";
+    public static final String INGREDIENTS = "api/ingredients";
+    public static final String ORDERS = "api/orders";
+    public static final String REGISTER = "api/auth/register";
+    public static final String LOGIN = "api/auth/login";
+    public static final String USER = "api/auth/user";
+    public static final String LOGOUT = "api/auth/logout";
+}
